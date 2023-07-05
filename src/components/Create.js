f80614739d90f761273fdcec2ebaea9204578669
rafreza <rafintokyo@gmail.com>
2023-07-05 01:26:11 -0400

    add post functionality, collections via firebase

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,19 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
+import { addDoc, collection } from "firebase/firestore";
+import { db, auth } from "../Firebase";
+import { useNavigate } from "react-router-dom";
 
 function Create(){
+
+  const [title, setTitle] = useState("");
+  const [postContent, setContent] = useState("");
+  const postDB = collection(db, "blogPosts");
+  let navigate = useNavigate();
+
+  const createPost = async () =>{
+    await addDoc(postDB, {
+      title,
+      postContent,
+      author: { name: auth.currentUser.displayName, id: auth.currentUser.uid }, 
+    });
+    navigate('/');
+  };
+
   return (
+
     <div className="postPage">
       <div className ="pageContainer">
         <h1>Create a Post</h1>
         <div className="inputContainer">
           <label> Title: </label>
-          <input placeholder="Keep it short!"/>
+          <input 
+            placeholder="Keep it short!"
+            onChange={(event) => {
+              setTitle(event.target.value);
+              }}
+          />
         </div>
         <div className="inputContainer">
           <label> Content: </label>
-          <input placeholder="Write your heart out..."/>
+          <textarea 
+            placeholder="Write your heart out..."
+            onChange={(event) => {
+              setContent(event.target.value);
+              }}
+          />
+        
         </div>
-        <button> Submit Post </button>
+        <button onClick={createPost}> Submit Post </button>
       </div>
     </div>
   );
