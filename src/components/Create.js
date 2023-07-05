8dba1530e7c2757b3c689dec7fbcaa1c09bee773
rafreza <rafintokyo@gmail.com>
2023-07-05 00:29:31 -0400

    build post creation base, no stylist

--- src/components/Create.js
+++ src/components/Create.js
@@ -1,7 +1,22 @@
 import React from "react";
 
 function Create(){
-  return <div>Create Post</div>;
+  return (
+    <div className="postPage">
+      <div className ="pageContainer">
+        <h1>Create a Post</h1>
+        <div className="inputContainer">
+          <label> Title: </label>
+          <input placeholder="Keep it short!"/>
+        </div>
+        <div className="inputContainer">
+          <label> Content: </label>
+          <input placeholder="Write your heart out..."/>
+        </div>
+        <button> Submit Post </button>
+      </div>
+    </div>
+  );
 }
 
 export default Create;
