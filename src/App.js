9e7bdf267d795015c3a6bf644c8510d9e54dd1b4
rafreza <rafintokyo@gmail.com>
2023-07-05 00:16:32 -0400

    finalize authentication, login logout functionality

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 import Home from './components/Home';
 import Login from './components/Login';
 import Create from './components/Create';
 import { useState } from 'react';
+import { signOut } from 'firebase/auth';
+import { auth } from './Firebase';
 
 function App() {
   const [isAuth, setIsAuth] = useState(false);
+  const out = () => {
+    signOut(auth).then(()=> {
+      localStorage.clear();
+      setIsAuth(false);
+      window.location.pathname = "/login";
+    });
+  }
   return (
     <Router>
       <nav>
         <Link to="/"> Home </Link>
         <Link to="/createpost"> Create Post </Link>
-        <Link to="/login"> Login </Link>
+        {!isAuth ? <Link to="/login"> Login </Link> : <Link to="/login"> Log Out </Link>}
       </nav>
       <Routes>
         <Route path="/" element={<Home />}/>
